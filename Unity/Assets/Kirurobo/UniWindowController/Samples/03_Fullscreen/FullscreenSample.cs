f7f7e6cbcdf142aeea17b1376e787f3baed119f0
Kirurobo <kirurobo@gmail.com>
2020-12-20 17:56:03 +0900

    サンプル03はフルスクリーンの切替とし、ModelViewerは04に移動した。

---
+++ Unity/Assets/Kirurobo/UniWindowController/Samples/03_Fullscreen/FullscreenSample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Kirurobo;
+using UnityEngine;
+
+namespace Kirurobo
+{
+    public class FullscreenSample : MonoBehaviour
+    {
+        private UniWindowController uniwin;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //UniWinCore.DebugMonitorInfo();
+
+            uniwin = GameObject.FindObjectOfType<UniWindowController>();
+
+            uniwin.OnStateChanged += () => { Debug.Log("State changed"); };
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
