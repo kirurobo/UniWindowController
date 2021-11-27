305435ee68d64c33b31ae66c3a89af6fec3db36c
Kirurobo <kirurobo@gmail.com>
2021-11-27 18:38:16 +0900

    Added SetAlphaValue to LibUniWinC

--- Runtime/Scripts/LowLevel/FilePanel.cs
+++ Runtime/Scripts/LowLevel/FilePanel.cs
@@ -88,20 +88,20 @@ namespace Kirurobo
         }
 
         /// <summary>
-        /// Dialog setting flags
+        /// ダイアログの設定フラグ
         /// </summary>
         [Flags]
         public enum Flag
         {
             None = 0,
-            //FileMustExist = 1,            // Windows only
-            //FolderMustExist = 2,          // Windows only
+            FileMustExist = 1,            // Windows only
+            FolderMustExist = 2,          // Windows only
             AllowMultipleSelection = 4,
-            //CanCreateDirectories = 16,    // macOS only
-            //OverwritePrompt = 256,        // Always enabled on macOS
-            //CreatePrompt = 512,           // Always enabled on macOS
+            CanCreateDirectories = 16,
+            OverwritePrompt = 256,        // Always enabled on macOS
+            CreatePrompt = 512,           // Always enabled on macOS
             ShowHiddenFiles = 4096,
-            //RetrieveLink = 8192,
+            RetrieveLink = 8192,
         }
 
         /// <summary>
@@ -176,7 +176,6 @@ namespace Kirurobo
         public static void OpenFilePanel(Settings settings, Action<string[]> action)
         {
             LibUniWinC.PanelSettings ps = new LibUniWinC.PanelSettings(settings);
-
             StringBuilder sb = new StringBuilder(pathBufferSize);
 
             if (LibUniWinC.OpenFilePanel(in ps, sb, (uint)sb.Capacity))
@@ -196,7 +195,6 @@ namespace Kirurobo
         public static void SaveFilePanel(Settings settings, Action<string[]> action)
         {
             LibUniWinC.PanelSettings ps = new LibUniWinC.PanelSettings(settings);
-
             StringBuilder sb = new StringBuilder(pathBufferSize);
 
             if (LibUniWinC.OpenSavePanel(in ps, sb, (uint)sb.Capacity))
