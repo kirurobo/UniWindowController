71ccfed1550842f7afc9cab8cff21acef43fe917
Kirurobo <kirurobo@gmail.com>
2021-12-04 12:33:46 +0900

    Samples were bundled because they require Common folder

--- UniWinC/Assets/Kirurobo/UniWindowController/Samples/04_FileDialog/FileDialogSample.cs
+++ UniWinC/Assets/Kirurobo/UniWindowController/Samples/04_FileDialog/FileDialogSample.cs
@@ -2,79 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FileDialogSample : MonoBehaviour
+namespace Kirurobo
 {
-    string message = "";
-
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Basic filepanel sample
+    /// </summary>
+    public class FileDialogSample : MonoBehaviour
     {
-        
-    }
+        string message = "";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnGUI()
-    {
-        if (GUI.Button(new Rect(10, 10, 160, 80), "Open a file"))
+        // Start is called before the first frame update
+        void Start()
         {
-            Kirurobo.FilePanel.Settings settings = new Kirurobo.FilePanel.Settings();
-            settings.filters = new Kirurobo.FilePanel.Filter[]
-            {
-                new Kirurobo.FilePanel.Filter("Image files", "png", "jpg", "jpeg", "tiff", "gif", "tga"),
-                new Kirurobo.FilePanel.Filter("Documents", "txt", "rtf", "doc", "docx"),
-                new Kirurobo.FilePanel.Filter("All files", "*")
-            };
-            settings.title = "Open a file!";
-            settings.initialDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyPictures);
 
-            message = "Canceled";
-            Kirurobo.FilePanel.OpenFilePanel(settings, (files) => {
-                message = "Open a file\n" + string.Join("\n", files);
-            });
         }
 
-        if (GUI.Button(new Rect(10, 100, 160, 80), "Open multiple files"))
+        // Update is called once per frame
+        void Update()
         {
-            Kirurobo.FilePanel.Settings settings = new Kirurobo.FilePanel.Settings();
-            settings.filters = new Kirurobo.FilePanel.Filter[]
-            {
-                new Kirurobo.FilePanel.Filter("Image files", "png", "jpg", "jpeg", "tiff", "gif", "tga"),
-                new Kirurobo.FilePanel.Filter("Documents", "txt", "rtf", "doc", "docx"),
-                new Kirurobo.FilePanel.Filter("All files", "*")
-            };
-            settings.flags = Kirurobo.FilePanel.Flag.AllowMultipleSelection;
-            settings.title = "Open multiple files!";
-            settings.initialDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
 
-            message = "Canceled";
-            Kirurobo.FilePanel.OpenFilePanel(settings, (files) => {
-                message = "Open multiple files\n" + string.Join("\n", files);
-            });
         }
 
-        if (GUI.Button(new Rect(10, 200, 160, 80), "Save file"))
+        // Show buttons and a textarea
+        private void OnGUI()
         {
-            Kirurobo.FilePanel.Settings settings = new Kirurobo.FilePanel.Settings();
-            settings.filters = new Kirurobo.FilePanel.Filter[]
+            if (GUI.Button(new Rect(10, 10, 160, 80), "Open a file"))
             {
-                new Kirurobo.FilePanel.Filter("Text file", "txt", "log"),
-                new Kirurobo.FilePanel.Filter("Image files", "png", "jpg", "jpeg", "tiff", "gif", "tga"),
-                new Kirurobo.FilePanel.Filter("All files", "*")
-            };
-            settings.title = "No save is actually performed";
-            settings.initialFile = "Test.txt";
+                FilePanel.Settings settings = new FilePanel.Settings();
+                settings.filters = new FilePanel.Filter[]
+                {
+                new FilePanel.Filter("Image files (*.png;*.jpg;*.jpeg;*.tiff;*.gif;*.tga)", "png", "jpg", "jpeg", "tiff", "gif", "tga"),
+                new FilePanel.Filter("Documents (*.txt;*.rtf;*.doc;*.docx)", "txt", "rtf", "doc", "docx"),
+                new FilePanel.Filter("All files", "*")
+                };
+                settings.title = "Open a file!";
+                settings.initialDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyPictures);
 
-            message = "Canceled";
-            Kirurobo.FilePanel.SaveFilePanel(settings, (files) => {
-                message = "Selected file\n" + string.Join("\n", files);
-            });
+                message = "Canceled";
+                FilePanel.OpenFilePanel(settings, (files) =>
+                {
+                    message = "Open a file\n" + string.Join("\n", files);
+                });
+            }
+
+            if (GUI.Button(new Rect(10, 100, 160, 80), "Open multiple files"))
+            {
+                FilePanel.Settings settings = new FilePanel.Settings();
+                settings.filters = new FilePanel.Filter[]
+                {
+                new FilePanel.Filter("Image files (*.png;*.jpg;*.jpeg;*.tiff;*.gif;*.tga)", "png", "jpg", "jpeg", "tiff", "gif", "tga"),
+                new FilePanel.Filter("Documents (*.txt;*.rtf;*.doc;*.docx)", "txt", "rtf", "doc", "docx"),
+                new FilePanel.Filter("All files", "*")
+                };
+                settings.flags = FilePanel.Flag.AllowMultipleSelection;
+                settings.title = "Open multiple files!";
+                settings.initialDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+
+                message = "Canceled";
+                FilePanel.OpenFilePanel(settings, (files) =>
+                {
+                    message = "Open multiple files\n" + string.Join("\n", files);
+                });
+            }
+
+            if (GUI.Button(new Rect(10, 200, 160, 80), "Save file"))
+            {
+                FilePanel.Settings settings = new FilePanel.Settings();
+                settings.filters = new FilePanel.Filter[]
+                {
+                new FilePanel.Filter("Text file (*.txt;*.log)", "txt", "log"),
+                new FilePanel.Filter("Image files (*.png;*.jpg;*.jpeg;*.tiff;*.gif;*.tga)", "png", "jpg", "jpeg", "tiff", "gif", "tga"),
+                new FilePanel.Filter("All files", "*")
+                };
+                settings.title = "No save is actually performed";
+                settings.initialFile = "Test.txt";
+
+                message = "Canceled";
+                FilePanel.SaveFilePanel(settings, (files) =>
+                {
+                    message = "Selected file\n" + string.Join("\n", files);
+                });
+            }
+
+            GUI.TextArea(new Rect(200, 10, 400, 400), message);
         }
-
-        GUI.TextArea(new Rect(200, 10, 400, 400), message);
     }
 }
