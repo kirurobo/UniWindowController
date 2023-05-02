f4f37eec1466db814e2c28af8b47c37388ff0207
Kirurobo <kirurobo@gmail.com>
2023-05-02 11:06:09 +0900

    Added quit button on SampleManager

--- UniWinC/Assets/Kirurobo/UniWindowController/Samples/00_Menu/SampleManager.cs
+++ UniWinC/Assets/Kirurobo/UniWindowController/Samples/00_Menu/SampleManager.cs
@@ -8,15 +8,6 @@ namespace Kirurobo
 {
     public class SampleManager : MonoBehaviour
     {
-        static readonly string[] _scenes =
-        {
-        "SampleMenu",
-        "SimpleSample",
-        "UiSample",
-        "FullScreenSample",
-        "FileDialogSample"
-    };
-
         private static SampleManager _instance;
         public static SampleManager Instance => _instance ?? (_instance = GameObject.FindObjectOfType<SampleManager>() ?? new SampleManager());
 
@@ -37,22 +28,20 @@ namespace Kirurobo
             SceneManager.sceneLoaded += SceneManager_sceneLoaded;
         }
 
+        /// <summary>
+        /// シーンロード時にメインカメラを記憶
+        /// </summary>
+        /// <param name="arg0"></param>
+        /// <param name="arg1"></param>
         private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
         {
             UniWindowController.current.SetCamera(Camera.main);
         }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
+        /// <summary>
+        /// 指定の名前のシーンを開く
+        /// </summary>
+        /// <param name="name">シーン名</param>
         public void LoadScene(string name)
         {
             if (name == "SimpleSample")
@@ -69,29 +58,16 @@ namespace Kirurobo
             SceneManager.LoadScene(name);
         }
 
-        //private void OnGUI()
-        //{
-        //    float y = 0;
-
-        //    for (int index = 0; index < _scenes.Length; index++)
-        //    {
-        //        if (GUI.Button(new Rect(0, y, 160, 36), _scenes[index]))
-        //        {
-        //            //if (index == 1)
-        //            //{
-        //            //    // SimpleSample の場合はスクリプトでの制御がないため、ここで透明化
-        //            //    UniWindowController.current.isTransparent = true;
-        //            //}
-        //            //else if (index == 3)
-        //            //{
-        //            //    // FullScreenSample の場合は強制的に最大化
-        //            //    UniWindowController.current.shouldFitMonitor = true;
-        //            //}
-
-        //            //SceneManager.LoadScene(_scenes[index]);
-        //        }
-        //        y += 40;
-        //    }
-        //}
+        /// <summary>
+        /// 終了
+        /// </summary>
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
     }
 }
